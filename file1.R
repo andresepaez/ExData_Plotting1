dd2cc170bdfb3337e66e9342d421de8461182601
andresepaez <andresepaez@gmail.com>
2020-09-04 07:25:28 -0500

    avance

--- file1.R
+++ file1.R
@@ -7,17 +7,22 @@ filetoread<-"household_power_consumption.txt"
 housepower<-fread(filetoread)
 workdirectory<-"D:/workspace/R/ExData_Plotting1"
 setwd(workdirectory)
-workdata<-mutate(housepower,Date=dmy(Date),Time=hms(Time))
-workdataf<-subset(workdata,(workdata$Date>="2007-02-01")&(workdata$Date<="2007-02-02"))
+
+workdata1<-housepower[complete.cases(housepower),]
+
+workdata2<-mutate(workdata1,DateTime=dmy_hms(paste(Date,Time)))
+
+workdata3<-subset(workdata2,(DateTime>=ymd_hms("2007-02-01 00:00:00"))&(DateTime<=ymd_hms("2007-02-02 23:59:59")))
+
+workdata4<-select(workdata3,-Date,-Time)
+
 #Ploi 1
-hist(as.numeric(workdataf$Global_active_power),col="red",main="Global Active Power",xlab="Global Active Power (kilowatts")
+hist(as.numeric(workdata4$Global_active_power),col="red",main="Global Active Power",xlab="Global Active Power (kilowatts")
 
 #Plot 2
-a<-as.numeric(workdataf$Global_active_power)
-b<-weekdays(workdataf$Date)
 
-with(workdataf,plot(as.numeric(Global_active_power),type="l"))
+with(workdata4,plot(as.numeric(Global_active_power)~DateTime,ylab="Global Active Power (kilowatts)",type="l"))
 
-with(workdataf,plot(y=as.numeric(Global_active_power),x=Date,type="l"))
+#Plot 3
 
-with(workdataf,plot(as.numeric(Global_active_power)~Time,type="l"))
+with(workdata4,plot(as.numeric(Global_active_power)~DateTime,ylab="Global Active Power (kilowatts)",type="l"))
